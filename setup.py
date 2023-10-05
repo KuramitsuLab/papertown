1a3c180df3dca50df98003c1b1c193b629062742
Kimio Kuramitsu <kkuramitsu@gmail.com>
2023-10-05 12:15:21 +0900

    fix:bugs

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def _requires_from_file(filename):
 
 setup(
     name="papertown",
-    version="0.0.10.2",
+    version="0.0.10.5",
     license="MIT",
     author="Kimio Kuramitsu",
     description="The PaperTown LLM Project",
